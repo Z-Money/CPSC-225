f3041b41f11edf90b15b0432c4f837b0ed59c211
Zachariah Kersey <zrun2008@gmail.com>
2026-09-23 10:58:17 -0400

    Added accesser and remover methods. Added test main method

--- PlaneFigureDBase.java
+++ PlaneFigureDBase.java
@@ -26,4 +26,47 @@ public class PlaneFigureDBase {
         return figure;
     }
 
+    public PlaneFigure getFigureByPosition(int index) {
+        if (index < 0 || index >= figureCount) {
+            return null;
+        }
+        return figures[index];
+    }
+
+    public PlaneFigure removeFigureByPosition(int index) {
+        if (index < 0 || index >= figureCount) {
+            return null;
+        }
+        PlaneFigure temp = figures[index];
+        figures[index] = figures[figureCount - 1];
+        figures[figureCount - 1] = null;
+        figureCount--;
+        return temp;
+    }
+
+    public static void main(String args[]) {
+        PlaneFigureDBase dbase = new PlaneFigureDBase(3);
+        dbase.addFigure(new Circle(10));
+        dbase.addFigure(new Circle(100));
+        dbase.addFigure(new Rectangle(10, 5));
+
+        System.out.println("Current Number of figures: " + dbase.getNumberOfFigures());
+        for (int i = 0; i < dbase.getNumberOfFigures(); i++) {
+            PlaneFigure pf = dbase.getFigureByPosition(i);
+            System.out.println(pf);
+        }
+
+        PlaneFigure pf = dbase.getFigureByPosition(2);
+        if (pf instanceof Circle) {
+            Circle circ = (Circle) pf;
+            System.out.println("Removed Figure: " + circ);
+            System.out.println("Diameter: " + circ.calcDiameter());
+        } else if (pf instanceof Rectangle) {
+            Rectangle rect = (Rectangle) pf;
+            System.out.println("Removed Figure: " + rect);
+            System.out.println("Length: " + rect.getLength());
+        } else {
+            System.out.println("No such figure.");
+        }
+    }
 }
